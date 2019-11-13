a450e4316e03eefeabbf85d0b7ff8bdfb0f96ec7
Adrien PHILIPPOT <adrienphilippot@gmail.com>
2019-11-13 12:34:17 +0100

    implementation footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,13 @@
 <template>
   <footer class="footer">
     <div class="contact_rgpd">
-      <img src="..\assets\logo_mon_traiteur.png" alt="logo_mon_traiteur" height="42" width="42"><p>Contact</p>
-      <img src="..\assets\logo_mon_traiteur.png" alt="logo_mon_traiteur" height="42" width="42"><p>RGPD</p>
+      <img src="..\assets\logo_mon_traiteur.png" alt="logo_mon_traiteur"><p>Contact</p>
+      <img src="..\assets\logo_mon_traiteur.png" alt="logo_mon_traiteur"><p>RGPD</p>
     </div>
     <div class="social_network_icons">
-      <img src="..\assets\logo_fb.png" alt="logo_mon_traiteur" height="42" width="42">
-      <img src="..\assets\logo_tw.png" alt="logo_mon_traiteur" height="42" width="42">
-      <img src="..\assets\logo_ig.png" alt="logo_mon_traiteur" height="42" width="42">
+      <img src="..\assets\logo_fb.png" alt="logo_mon_traiteur">
+      <img src="..\assets\logo_tw.png" alt="logo_mon_traiteur">
+      <img src="..\assets\logo_ig.png" alt="logo_mon_traiteur">
     </div>
   </footer>
 </template>
@@ -20,12 +20,37 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.contact_rgpd div
+
+.footer{
+  width: 100%;
+  height:4em;
+  background-color: #E6E5E5;
+}
+
+footer div{
+  display: inline;
+}
+
+img{
+  width: 2em;
+  height: 2em;
+}
+
+.contact_rgpd{
+  float: left;
+}
+
 .contact_rgpd p {
   display: inline;
 }
 
 .social_network_icons{
-  display: inline;
+  float: right;
 }
+
+.social_network_icons img{
+  display: inline;
+  margin-right: 1em;
+  vertical-align: auto;
+  }
 </style>
--- src/components/TestAdrien.vue
+++ src/components/TestAdrien.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <h1>Adrien</h1>
     <Footer/>
   </div>
 </template>
