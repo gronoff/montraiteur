497bd4b3de9633ccca4389a709e787d91f534bfc
Adrien PHILIPPOT <adrienphilippot@gmail.com>
2019-11-13 12:02:27 +0100

    new logos, add adrien's component

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer class="footer">
+    <div class="contact_rgpd">
+      <img src="..\assets\logo_mon_traiteur.png" alt="logo_mon_traiteur" height="42" width="42"><p>Contact</p>
+      <img src="..\assets\logo_mon_traiteur.png" alt="logo_mon_traiteur" height="42" width="42"><p>RGPD</p>
+    </div>
+    <div class="social_network_icons">
+      <img src="..\assets\logo_fb.png" alt="logo_mon_traiteur" height="42" width="42">
+      <img src="..\assets\logo_tw.png" alt="logo_mon_traiteur" height="42" width="42">
+      <img src="..\assets\logo_ig.png" alt="logo_mon_traiteur" height="42" width="42">
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.contact_rgpd div
+.contact_rgpd p {
+  display: inline;
+}
+
+.social_network_icons{
+  display: inline;
+}
+</style>
--- src/components/TestAdrien.vue
+++ src/components/TestAdrien.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <h1>Adrien</h1>
+    <Footer/>
   </div>
 </template>
 
 <script>
+import Footer from "./Footer"
 export default {
-  name: 'TestAdrien'
+  name: 'TestAdrien',
+  components: {
+    Footer
+  }
 }
 </script>
