2ef7347e16c361ecb77c79462b399e51851ba688
dimitri <dimitri@gronoff.com>
2019-11-13 15:53:25 +0100

    ok super global

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,23 @@
 <template>
   <footer class="footer">
     <div class="contact_rgpd">
-      <img src="../assets/logo_mon_traiteur.png" alt="logo_mon_traiteur" height="42" width="42"><p>Contact</p>
-      <img src="../assets/logo_mon_traiteur.png" alt="logo_mon_traiteur" height="42" width="42"><p>RGPD</p>
+      <a href="url">
+        <span>
+          <img src="../assets/img/logo_mon_traiteur.png" alt="logo_mon_traiteur">
+          <p>Contact</p>
+        </span>
+      </a>
+      <a href="url">
+        <span>
+          <img src="../assets/img/logo_mon_traiteur.png" alt="logo_mon_traiteur">
+          <p>RGPD</p>
+        </span>
+      </a>
     </div>
     <div class="social_network_icons">
-      <img src="../assets/logo_fb.png" alt="logo_mon_traiteur" height="42" width="42">
-      <img src="../assets/logo_tw.png" alt="logo_mon_traiteur" height="42" width="42">
-      <img src="../assets/logo_ig.png" alt="logo_mon_traiteur" height="42" width="42">
+      <img src="../assets/img/logo_fb.png" alt="logo facebook">
+      <img src="../assets/img/logo_tw.png" alt="logo twitter">
+      <img src="../assets/img/logo_ig.png" alt="logo">
     </div>
   </footer>
 </template>
@@ -20,41 +30,33 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 p{
   color:#7A7D7D;
 }
-
 .footer{
   width: 100%;
   height:4em;
   background-color: #E6E5E5;
 }
-
 footer div{
   display: inline;
 }
-
 img{
   width: 2em;
   height: 2em;
 }
-
 span{
   margin-left: 1em;
   margin-right: 1%;
   margin-top: 1em;
 }
-
 .contact_rgpd p {
   display: inline;
 }
-
 .social_network_icons{
   float: right;
   margin-top: 1em;
 }
-
 .social_network_icons img{
   display: inline;
   margin-right: 1em;
