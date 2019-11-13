5a640efceaa744ebba7994a5072c760355dbcdd8
Adrien PHILIPPOT <adrienphilippot@gmail.com>
2019-11-13 13:35:14 +0100

    fix chemin images

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,13 @@
 <template>
   <footer class="footer">
     <div class="contact_rgpd">
-      <img src="..\assets\logo_mon_traiteur.png" alt="logo_mon_traiteur"><p>Contact</p>
-      <img src="..\assets\logo_mon_traiteur.png" alt="logo_mon_traiteur"><p>RGPD</p>
+      <img src="../assets/logo_mon_traiteur.png" alt="logo_mon_traiteur"><p>Contact</p>
+      <img src="../assets/logo_mon_traiteur.png" alt="logo_mon_traiteur"><p>RGPD</p>
     </div>
     <div class="social_network_icons">
-      <img src="..\assets\logo_fb.png" alt="logo_mon_traiteur">
-      <img src="..\assets\logo_tw.png" alt="logo_mon_traiteur">
-      <img src="..\assets\logo_ig.png" alt="logo_mon_traiteur">
+      <img src="../assets/logo_fb.png" alt="logo_mon_traiteur">
+      <img src="../assets/logo_tw.png" alt="logo_mon_traiteur">
+      <img src="../assets/logo_ig.png" alt="logo_mon_traiteur">
     </div>
   </footer>
 </template>
