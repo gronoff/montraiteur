04ca3fa1d6d4ac1a0e1e6453838a2e9602a103f4
Audie <audie.menut@gmail.com>
2019-11-13 12:34:54 +0100

    composants Form & InputText

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="inputForm">
-    <label for="">Enter your name: </label>
-    <input type="text" name="" id="" required>
+    <label :for="id">{{ label }}</label>
+    <input type="text" :name="id" :id="id" required>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'InputText'
+  name: 'InputText',
+  props: {
+    label: String,
+    id: String
+  }
 }
 </script>
+
+<style scoped>
+  .inputForm {
+    margin-left: 14px;
+  }
+  label {
+    display: block;
+    font-size: 14px;
+    line-height: 24px;
+    color: #7A7D7D;
+  }
+  input {
+    width: 300px;
+    height: 30px;
+    background: #FFFBFE;
+    border-radius: 5px;
+    border: none;
+  }
+</style>
--- src/components/TestAude.vue
+++ src/components/TestAude.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <h1>Aude</h1>
+    <InscriptionForm />
   </div>
 </template>
 
 <script>
+import InscriptionForm from './InscriptionForm'
+
 export default {
-  name: 'TestDimitri'
+  name: 'TestAude',
+  components: {
+    InscriptionForm
+  }
 }
 </script>
