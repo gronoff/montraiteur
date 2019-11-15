c73675285600936f6e1ad446a36deb55c1d1cf57
Adrien PHILIPPOT <adrienphilippot@gmail.com>
2019-11-15 08:59:35 +0100

    add cards dynamiques

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,31 +1,69 @@
 <template>
-  <div class="card">
-    <img src="https://img.freepik.com/free-photo/crop-couple-cooking-salad-together_23-2148114200.jpg?size=626&ext=jpg" alt={}>
-    <div>
-     <h4>Titre</h4>
-     <p>description du traiteur provenant du fichier json </p> 
+  <div class="cards">
+    <div v-for="(card, id) of cards" :key="id">
+      <img :src="card.lien_image">
+      <div>
+        <h4>{{ card.titre }}</h4>
+        <p>{{ card.description }}</p> 
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'card'
+  name: 'card',
+    props: {
+    id: Number,
+    titre: String,
+    lien_image: String,
+    description: String,
+  },
+  data: function(){
+    return{
+    cards: 
+        [
+            {
+                id: 1,
+                titre: "Aux délices d’Ignacio",
+                description: "La Catalogne dans vos assiettes. Un délice pour vos papilles. Un festival de saveurs. Vous serez transportés au delà de vos espérances dans une farandole de gouts. Plaisir ibérique assuré !",
+                lien_image: "http://4.bp.blogspot.com/-nJP4ELRi6lo/U3uee1cTjQI/AAAAAAAACLo/CMYmGaUnzCM/s1600/PAELLA.jpg",
+                note_prix: 2,
+                note_qualite: 4
+            },
+            {
+                id: 2,
+                titre: "Parigo Bobo Burgers Traiteur",
+                description: "C'est cher, c'est pas bon, il n'y a rien à manger, c'est bobo, c'est Parisien, donc ça ne peut que marcher... Venez lâcher votre flooz les pigeons !",
+                lien_image: "http://www.ldc-halal.com/ressources/images-restaurants/restaurant-214-2.jpg?version=636245852231970000",
+                note_prix: 0,
+                note_qualite: 0
+            },        {
+                id: 3,
+                titre: "Nagasaki maki",
+                description: "Un cocktail détonnant, une explosion de saveurs, vos convives seront irradiés !",
+                lien_image: "https://i.f1g.fr/media/figarofr/orig/2011/04/21/e515107c-6c25-11e0-abde-d3e0c2a59365.jpg",
+                note_prix: 3,
+                note_qualite: 3
+            }
+        ]
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-.card{
-  height: 200px;
+.cards div{
+  height: 180px;
   background-color: #F1F1F1;
   border-radius: 10px;
   padding: 2%;
   vertical-align: center;
 }
 
-.card div{
+.cards div div{
   width: 75%;
   height: 100%;
   background-color: #FFFBFE;
@@ -33,13 +71,13 @@ export default {
   border-radius: 10px;
 }
 
-.card p, h4{
+.cards div p, h4{
   margin-left: 2%;
 }
 
-.card img{
+.cards div img{
   float: left;
-  width: 15%;
+  width: 20%;
   border-radius: 10px;
 }
 
