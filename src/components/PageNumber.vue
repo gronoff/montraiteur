8dbe823f15b172f1d2751887384932bb3986d81a
NinonTz <ninon.touzeau@gmail.com>
2019-11-19 12:14:00 +0100

    first commit. Modif du router. Ajout des composants pageNumber et TestNinon

---
+++ src/components/PageNumber.vue
@@ -0,0 +1,37 @@
+<template>
+    <p> &lt; {{txt}} &gt; </p>
+</template>
+        
+<script>
+    export default {
+      name: 'PageNumber',
+      props: {
+          txt: String
+      }
+    }
+</script>
+        
+<style scoped>
+  p {
+    padding: 8px 15px 6px 15px;
+    /*box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);*/
+    border:0 none;
+    cursor:pointer;
+    font-family: karma;
+    font-size: 11px;
+    color: var(--grisFonce);
+    transition: background 500ms;
+    width: 90px;
+    height: 30px;
+    /*border-radius: 60px / 50px;*/
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  p:hover {
+    background: var(--grisClair);
+  }
+  p:active {
+    box-shadow: none;
+  }
+</style>
---
+++ src/components/TestNinon.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Neewee</h1>
+    <PageNumber txt="page 2 sur 7"/>
+  </div>
+</template>
+
+<script>
+import PageNumber from './PageNumber.vue'
+export default {
+  name: 'TestNinon',
+  components:{PageNumber}
+}
+</script>
