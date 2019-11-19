4def7d063d716670b99d56598fee1fdc0ae51058
Adrien PHILIPPOT <adrienphilippot@gmail.com>
2019-11-19 11:43:02 +0100

    add bouton heart fix placement des boutons dans les cards

---
+++ src/components/HeartButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button v-on:click="addToFavorite()"><img :src="lien_src" alt="bouton favoris"></button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonHeart',
+  data: function(){
+    return {
+      image_active: require("@/assets/img/icon_heart_active.png"),
+      image_unactive: require("@/assets/img/icon_heart_unactive.png"),
+      lien_src: require("@/assets/img/icon_heart_unactive.png")
+    }
+  },
+  methods:{
+    addToFavorite: function(){
+      if (this.lien_src == this.image_unactive){
+        this.lien_src = this.image_active;
+      }
+      else if(this.lien_src ==  this.image_active){
+        this.lien_src = this.image_unactive;
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+button{
+  background-color: transparent;
+  border: 0 none;
+  cursor: pointer;
+}
+
+img{
+  width: 2em;
+  height: 2em;
+}
+
+</style>
