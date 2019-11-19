114699f77ee6ed4ef699d2546266af3802cad68c
NinonTz <ninon.touzeau@gmail.com>
2019-11-19 15:36:25 +0100

    creation jumbotron

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,21 @@
+<template>
+    <a target="_blank" href="/">
+    <img
+      src="../assets/img/jumbotron.jpg"
+      alt="logo_mon_traiteur"
+    />
+    </a>
+</template>
+
+<script>
+export default {
+    name: "Jumbotron"
+}
+</script>
+
+<style scoped>
+    img {
+       width: 1440px;
+       height: 242px; 
+    }
+</style>
--- src/components/TestNinon.vue
+++ src/components/TestNinon.vue
@@ -2,13 +2,15 @@
   <div>
     <h1>Neewee</h1>
     <PageNumber txt="page 2 sur 7"/>
+    <Jumbotron/>
   </div>
 </template>
 
 <script>
 import PageNumber from './PageNumber.vue'
+import Jumbotron from './Jumbotron.vue'
 export default {
   name: 'TestNinon',
-  components:{PageNumber}
+  components:{PageNumber, Jumbotron}
 }
 </script>
