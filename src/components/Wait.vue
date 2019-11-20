230dfb4b6c05022850221ed5b11b0f3fea2f849a
Audie <audie.menut@gmail.com>
2019-11-20 09:16:41 +0100

    ajout du veuillez patienter

---
+++ src/components/Wait.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Header />
+    <div>
+      <img src="https://gifimage.net/wp-content/uploads/2018/11/gif-veuillez-patienter.gif" />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './Header'
+import Footer from './Footer'
+
+export default {
+  name: 'Wait',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style scoped>
+  img {
+    display: block;
+    width: 40vw;
+    min-width: 250px;
+    margin: 0 auto 30px auto;
+  }
+</style>
